053557305bd233b74fb64371762d2852d6f3e1cf
lakshmig <lakshmig@LAP032.stratapps.com>
2019-04-08 15:31:22 +0530

    email commit 1

--- xamplifylive/src/main/java/xamplifyLive/EmailCampaign.java
+++ xamplifylive/src/main/java/xamplifyLive/EmailCampaign.java
@@ -10,8 +10,7 @@ package xamplifyLive;
 
 	import org.openqa.selenium.By;
 	import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebDriver;
 	import org.openqa.selenium.WebElement;
 	import org.openqa.selenium.interactions.Actions;
 	import org.openqa.selenium.support.ui.Select;
